b59be86d07fd4a35f0a1ce44fb9fc8b4ecde86df
phonkee <phonkee@phonkee.eu>
2025-03-23 02:35:20 +0100

    Updates
    * action.JsonPath initial commit

--- tester/action/domain.go
+++ tester/action/domain.go
@@ -43,6 +43,8 @@ const (
 
 // Action is an action that can be performed on the response. It has currently 2 meanings: Assertion and Unmarshal.
 // TODO: add path or id to run so we can print better error messages?
+//
+//go:generate mockery --name Action --filename action.go
 type Action interface {
 	Run(t require.TestingT, ctx context.Context, value any, raw json.RawMessage, err error) error
 	Value(t require.TestingT) (any, bool)
